cf13ab66499a271e4b7006830fab625bd44805c4
Charlie Weiss <charlotte.weiss@students.olin.edu>
2018-11-16 09:50:30 -0500

    New main

--- Main.ino
+++ Main.ino
@@ -1,14 +1,11 @@
 /*
   LiquidCrystal Library - Hello World
-
   Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
   library works with all LCD displays that are compatible with the
   Hitachi HD44780 driver. There are many of them out there, and you
   can usually tell them by the 16-pin interface.
-
   This sketch prints "Hello World!" to the LCD
   and shows the time.
-
   The circuit:
    LCD RS pin to digital pin 12
    LCD Enable pin to digital pin 11
@@ -22,7 +19,6 @@
    10K resistor:
    ends to +5V and ground
    wiper to LCD VO pin (pin 3)
-
   Library originally added 18 Apr 2008
   by David A. Mellis
   library modified 5 Jul 2009
@@ -33,18 +29,21 @@
   by Tom Igoe
   modified 7 Nov 2016
   by Arturo Guadalupi
-
   This example code is in the public domain.
-
   http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
 */
 
 int bean_level = A5;
 int water_level = A4;
 int cup_level = A3;
-
+int pour_pin = 6;
+int grind_pin = 5;
+long current_time;
+long start_time;
+long go_grind = 1000;
+long grind_time = 10000;
 bool stocked = false;
+bool set_timer = false;
 
 
 // include the library code:
@@ -65,11 +64,13 @@ void setup() {
   pinMode(bean_level, INPUT);
   pinMode(water_level, INPUT);
   pinMode(cup_level, INPUT);
+  pinMode(pour_pin, OUTPUT);
+  pinMode(grind_pin, OUTPUT);
 }
 
-void loop() {
+void check_status() {
 
-  Serial.println(stocked);
+//  Serial.println(stocked);
 
   if (analogRead(bean_level) > 500) {
     lcd.print("Refill beans pls");
@@ -87,6 +88,7 @@ void loop() {
     lcd.print("Ready to brew      ");
     stocked = true;
   }
+  
 
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
@@ -94,3 +96,44 @@ void loop() {
   // print the number of seconds since reset:
   //lcd.print(millis() / 1000);
 }
+
+void brew_coffee(){
+  current_time = millis();
+//  Serial.println(current_time - start_time);
+  if (current_time - start_time < go_grind){
+    digitalWrite(grind_pin, HIGH);
+    Serial.println("Grinding");
+    }
+  else if (current_time - start_time <= grind_time){
+    digitalWrite(grind_pin,LOW);
+    } 
+  else if (current_time - start_time <= grind_time+5000){
+    digitalWrite(pour_pin,HIGH);
+    Serial.println("Pouring");
+    }
+  else if (current_time - start_time >= grind_time+5000){
+    digitalWrite(pour_pin,LOW);
+    Serial.println("Finished");
+    }  
+  }
+
+void reset(){
+  set_timer = false;
+  }
+
+void loop() {
+  check_status();
+  current_time = millis();
+  if (stocked == true){
+//  Serial.println("stocked");
+    if (set_timer == false){
+      start_time = millis();
+      set_timer = true;
+      }
+      brew_coffee();
+  }
+  else {
+    Serial.println("Check machine status");
+    digitalWrite(grind_pin,LOW);
+    }
+  }
